564c69b2f39abc94ef2f7942bdc42cfc001cb226
Jefid <99925114+Jefid@users.noreply.github.com>
2022-03-23 16:18:40 -0400

    start button functionallity

--- develop/script.js
+++ develop/script.js
@@ -1,22 +1,23 @@
 //array of objects storing questions and answers
+
 var questions = [
-    {question: "",
+    {question: "Question #1",
     answers:["", "", "", ""],
     correct: ""},
 
-    {question: "",
+    {question: "Question #2",
     answers:["", "", "", ""],
     correct: ""},
 
-    {question: "",
+    {question: "Question #3",
     answers:["", "", "", ""],
     correct: ""},
 
-    {question: "",
+    {question: "Question #4",
     answers:["", "", "", ""],
     correct: ""},
 
-    {question: "",
+    {question: "Question #5",
     answers:["", "", "", ""],
     correct: ""}]
 
@@ -28,21 +29,38 @@ var player = {};
 
 //displays question
 function showQuestion() {
-    getElementById("question").text(questions[questionLog].question)
-    getElementById("answer-1").text(questions[questionLog].answers[0])
-    getElementById("answer-2").text(questions[questionLog].answers[1])
-    getElementById("answer-3").text(questions[questionLog].answers[2])
-    getElementById("answer-4").text(questions[questionLog].answers[3])
-}
+    console.log(document.getElementById("question").innerHTML);
 
+    document.getElementById("question").innerHTML = (questions[questionLog].question);
+    document.getElementById("answer-1").innerHTML = (questions[questionLog].answers[0]);
+    document.getElementById("answer-2").innerHTML = (questions[questionLog].answers[1]);
+    document.getElementById("answer-3").innerHTML = (questions[questionLog].answers[2]);
+    document.getElementById("answer-4").innerHTML = (questions[questionLog].answers[3]);
+};
+//countdown function
 function countdown() {
-    $("#counter").text(counter);
+    document.getElementById("counter").innerHTML = (counter);
     timer = setInterval(function() {
         counter--;
-        $("#counter").text(counter);
+        getElementById("counter").text(counter);
         if (counter === 0) {
             clearInterval(timer);
             endGame();
         }
     }, 1000);
 }
+// //function to end quiz/ enter initials for score
+function endGame(){
+
+}
+//funstion to start quiz
+function startGame(){
+    var quizScreen= document.getElementById("quizSection");
+    var quizInstructions= document.getElementById("quizDirections");
+    quizInstructions.classList.add("invisible");
+    quizScreen.classList.remove("invisible");
+    showQuestion();
+}
+
+//need to have a click event to check questions 
+document.getElementById("startButton").addEventListener("click", startGame);
